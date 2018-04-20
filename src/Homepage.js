ea0ab8158008c386492de0e1daba16c65f5f4b92
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-20 11:21:10 -0700

    Homepage.

--- src/Homepage.js
+++ src/Homepage.js
@@ -4,6 +4,9 @@ class Homepage extends Component {
   render() {
     return (
       <div>
+        <h2>Working</h2>
+        <h2>Not yet working</h2>
+        <p>Everything else 😬 Please fork <a href='https://github.com/bigearth/query.cash'>the repo</a> and help us build it</p>
       </div>
     );
   }
