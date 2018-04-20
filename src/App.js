c788fc01f3d1884c4da3b9c5d02e0a09147e99c9
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-20 11:12:05 -0700

    More stub templates.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   NavLink
 } from 'react-router-dom';
 
+import Homepage from './Homepage';
 import AbandonTransaction from './components/AbandonTransaction';
 import AddMultisigAddress from './components/AddMultisigAddress';
 import AddNode from './components/AddNode';
@@ -50,7 +51,14 @@ import GetTxOut from './components/GetTxOut';
 import GetTxOutProof from './components/GetTxOutProof';
 import GetUnconfirmedBalance from './components/GetUnconfirmedBalance';
 import GetWalletInfo from './components/GetWalletInfo';
-import Homepage from './Homepage';
+import Help from './components/Help';
+import KeypoolRefill from './components/KeypoolRefill';
+import ListAccounts from './components/ListAccounts';
+import ListAddressGroupings from './components/ListAddressGroupings';
+import ListLockUnspent from './components/ListLockUnspent';
+import ListReceivedByAccount from './components/ListReceivedByAccount';
+import ListReceivedByAddress from './components/ListReceivedByAddress';
+import ListTransactions from './components/ListTransactions';
 import SetTxFee from './components/SetTxFee';
 import SignMessage from './components/SignMessage';
 import SignRawTransaction from './components/SignRawTransaction';
@@ -433,6 +441,70 @@ class App extends Component {
                   <NavLink
                     activeClassName="pure-menu-selected"
                     className="pure-menu-link"
+                    to="/help">
+                    Help
+                  </NavLink>
+                </li>
+                <li className="pure-menu-item">
+                  <NavLink
+                    activeClassName="pure-menu-selected"
+                    className="pure-menu-link"
+                    to="/keypoolrefill">
+                    KeypoolRefill
+                  </NavLink>
+                </li>
+                <li className="pure-menu-item">
+                  <NavLink
+                    activeClassName="pure-menu-selected"
+                    className="pure-menu-link"
+                    to="/listaccounts">
+                    ListAccounts
+                  </NavLink>
+                </li>
+                <li className="pure-menu-item">
+                  <NavLink
+                    activeClassName="pure-menu-selected"
+                    className="pure-menu-link"
+                    to="/listaddressgroupings">
+                    ListAddressGroupings
+                  </NavLink>
+                </li>
+                <li className="pure-menu-item">
+                  <NavLink
+                    activeClassName="pure-menu-selected"
+                    className="pure-menu-link"
+                    to="/listlockunspent">
+                    ListLockUnspent
+                  </NavLink>
+                </li>
+                <li className="pure-menu-item">
+                  <NavLink
+                    activeClassName="pure-menu-selected"
+                    className="pure-menu-link"
+                    to="/listreceivedbyaccount">
+                    ListReceivedByAccount
+                  </NavLink>
+                </li>
+                <li className="pure-menu-item">
+                  <NavLink
+                    activeClassName="pure-menu-selected"
+                    className="pure-menu-link"
+                    to="/listreceivedbyaddress">
+                    ListReceivedByAddress
+                  </NavLink>
+                </li>
+                <li className="pure-menu-item">
+                  <NavLink
+                    activeClassName="pure-menu-selected"
+                    className="pure-menu-link"
+                    to="/listtransactions">
+                    ListTransactions
+                  </NavLink>
+                </li>
+                <li className="pure-menu-item">
+                  <NavLink
+                    activeClassName="pure-menu-selected"
+                    className="pure-menu-link"
                     to="/settxfee">
                     SetTxFee
                   </NavLink>
@@ -556,6 +628,14 @@ class App extends Component {
             <Route path="/gettxoutproof" component={GetTxOutProof}/>
             <Route path="/getunconfirmedbalance" component={GetUnconfirmedBalance}/>
             <Route path="/getwalletinfo" component={GetWalletInfo}/>
+            <Route path="/help" component={Help}/>
+            <Route path="/keypoolrefill" component={KeypoolRefill}/>
+            <Route path="/listaccounts" component={ListAccounts}/>
+            <Route path="/listaddressgroupings" component={ListAddressGroupings}/>
+            <Route path="/listlockunspent" component={ListLockUnspent}/>
+            <Route path="/listreceivedbyaccount" component={ListReceivedByAccount}/>
+            <Route path="/listreceivedbyaddress" component={ListReceivedByAddress}/>
+            <Route path="/listtransactions" component={ListTransactions}/>
             <Route path="/settxfee" component={SetTxFee}/>
             <Route path="/signmessage" component={SignMessage}/>
             <Route path="/signrawtransaction" component={SignRawTransaction}/>
