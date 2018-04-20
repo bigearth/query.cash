65e1fc0fa2ceb7a29c8d2be5f0ead9983c56c826
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-19 22:34:45 -0700

    decode raw tx.

--- src/DecodeRawTransaction.js
+++ src/DecodeRawTransaction.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { ocean } from 'react-syntax-highlighter/styles/hljs';
+
+let BITBOXCli = require('bitbox-cli/lib/bitboxcli').default;
+let BITBOX = new BITBOXCli({
+  protocol: 'http',
+  host: '138.68.54.100',
+  port: 8332,
+  username: 'bitcoin',
+  password: 'xhFjluMJMyOXcYvF',
+  corsproxy: true
+});
 
 class DecodeRawTransaction extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      txid: '',
+      hex: '',
       data: ''
     };
   }
@@ -12,12 +24,12 @@ class DecodeRawTransaction extends Component {
   handleInputChange(e) {
     let value = e.target.value;
     this.setState({
-      txid: value
+      hex: value
     });
   }
 
   handleSubmit(e) {
-    BITBOX.RawTransactions.getRawTransaction(this.state.txid).then((result) => {
+    BITBOX.RawTransactions.decodeRawTransaction(this.state.hex).then((result) => {
       this.setState({
         data: result
       })
@@ -30,17 +42,17 @@ class DecodeRawTransaction extends Component {
     return (
       <div className="DecodeRawTransaction">
         <h1 className="DecodeRawTransaction-title">DecodeRawTransaction</h1>
-        <form className="pure-form pure-form-aligned">
+        <form className="pure-form pure-form-aligned" onSubmit={this.handleSubmit.bind(this)}>
             <fieldset>
                 <div className="pure-control-group">
-                    <label>Username</label>
-                    <input id="name" type="text" placeholder="Username"/>
-                    <span className="pure-form-message-inline">This is a required field.</span>
+                    <label>Raw Hex</label>
+                    <input onChange={this.handleInputChange.bind(this)} id="name" type="text" placeholder="Raw Hex"/>
                 </div>
                 <div>
                     <button type="submit" className="pure-button pure-button-primary">Submit</button>
                 </div>
             </fieldset>
+            <SyntaxHighlighter language='javascript' style={ocean}>{this.state.data}</SyntaxHighlighter>
         </form>
       </div>
     );
