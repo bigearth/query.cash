4259579ee92c3a2395f683c917a6481f849f1719
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-25 22:16:46 -0500

    https

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
 
     let BITBOXCli = require('bitbox-cli/lib/bitboxcli').default;
     let BITBOX = new BITBOXCli({
-      protocol: 'http',
+      protocol: 'https',
       host: '138.68.54.100',
       port: 8332,
       username: 'bitcoin',
